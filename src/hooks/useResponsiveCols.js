3f2fffc1d6c0a4fefcd57b8ff8df8d286aa1eda9
Seedy <seedy.dupuis@gmail.com>
2021-09-16 01:33:18 +0200

    responsive: better hooks

--- src/hooks/useResponsiveCols.js
+++ src/hooks/useResponsiveCols.js
@@ -1,22 +1,33 @@
 import useIsXs from 'hooks/useIsXs';
 import useIsDownSm from 'hooks/useIsDownSm';
+import useIsDownMd from 'hooks/useIsDownMd';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useMemo } from 'react';
 
 export default () => {
   const isXs = useIsXs();
   const isDownSm = useIsDownSm();
+  const isDownMd = useIsDownMd();
+
+  const isUpLargeSm = useMediaQuery('(min-width: 780px) and (max-width: 960px)');
 
   const cols = useMemo(
     () => {
       if (isXs) {
         return 2;
       }
+      if (isUpLargeSm) {
+        return 4;
+      }
       if (isDownSm) {
         return 3;
       }
-      return 4;
+      if (isDownMd) {
+        return 5;
+      }
+      return 6;
     },
-    [isXs, isDownSm],
+    [isXs, isDownSm, isUpLargeSm, isDownMd],
   );
 
   return cols;
