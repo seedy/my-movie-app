fa575a58f00cbc6ad7579a43681f0ff0d8626ec6
Seedy <seedy.dupuis@gmail.com>
2021-09-15 17:41:00 +0200

    fetch: fetch popular and upcoming movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
+import { DONE } from 'hooks/useFetch';
+
 import objectToCamelCaseMap from 'helpers/objectToCamelCase/map';
 
-import { useFetchTMDBUpcoming } from 'hooks/useFetch/tmdb';
-import { useEffect } from 'react';
+import { useFetchTMDBUpcoming, useFetchTMDBPopular } from 'hooks/useFetch/tmdb';
+import { useCallback, useEffect, useMemo } from 'react';
 
+import CircularProgress from '@material-ui/core/CircularProgress';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import IconButton from '@material-ui/core/IconButton';
+
+import CheckIcon from '@material-ui/icons/Check';
 
 // COMPONENTS
 function App() {
-  const [status, callback] = useFetchTMDBUpcoming(objectToCamelCaseMap);
+  const [upcomingStatus, upcomingCallback] = useFetchTMDBUpcoming(objectToCamelCaseMap);
+  const [popularStatus, popularCallback] = useFetchTMDBPopular(objectToCamelCaseMap);
+
+  const upcomingDone = useMemo(
+    () => upcomingStatus === DONE,
+    [upcomingStatus],
+  );
+
+  const popularDone = useMemo(
+    () => popularStatus === DONE,
+    [popularStatus],
+  );
+
+  const allMoviesDone = useMemo(
+    () => upcomingDone && popularDone,
+    [upcomingDone, popularDone],
+  );
+
+  const allMoviesCallback = useCallback(
+    () => Promise.allSettled([
+      upcomingCallback(),
+      popularCallback(),
+    ]),
+    [upcomingCallback, popularCallback],
+  );
 
   useEffect(
     () => {
-      callback();
+      allMoviesCallback();
     },
-    [callback],
+    [allMoviesCallback],
   );
 
   return (
     <>
       <CssBaseline />
-      {status}
+      {allMoviesDone ? (
+        <IconButton>
+          <CheckIcon />
+        </IconButton>
+      ) : (
+        <CircularProgress />
+      )}
     </>
   );
 }
