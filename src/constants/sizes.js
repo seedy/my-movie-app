4feb71e61a202377e5f3caa2715bb07814cdc675
Seedy <seedy.dupuis@gmail.com>
2021-09-16 01:28:41 +0200

    constants: factorize sizing and rating

---
+++ src/constants/sizes.js
@@ -0,0 +1,8 @@
+export const ROW_HEIGHT = 300;
+export const FIXED_WIDTH = 200;
+
+export const POSTER_BEST_SIZE = 'w185';
+export const POSTER_FALLBACK_SIZE = 'original';
+
+export const BACKDROP_BEST_SIZE = 'w1280';
+export const BACKDROP_FALLBACK_SIZE = 'original';
